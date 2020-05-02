bc6a4655b4c945361f4276d0915eb18305184eeb
Mauricio.carias <mauricio.carias1@outlook.com>
2020-05-01 23:05:03 -0600

    Primer Commit

---
+++ 2014gc604parcialACC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2014gc604parcialACC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
